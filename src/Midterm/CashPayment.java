3c0ff913946d512d0d9e875a5c281ebd00bf704d
sam <samantha.underwood@gmail.com>
2022-05-25 20:36:03 -0400

    First commit for SamsBranch

--- src/Midterm/CashPayment.java
+++ src/Midterm/CashPayment.java
@@ -1,6 +1,11 @@
 package Midterm;
 
 public class CashPayment extends Payment {
+
+	public CashPayment(double subtotal, double salesTax, double taxRate, double total) {
+		super(subtotal, salesTax, taxRate, total);
+		// TODO Auto-generated constructor stub
+	}
 	
 	//Fields:
 	//amountTendered
